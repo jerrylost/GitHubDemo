a69b7e0e5130637aef1ce3422703825483911323
jerrylost <cecszj@163.com>
2016-01-09 10:40:50 +0800

    添加新的Activity

--- app/src/main/java/com/jerry/githubdemo/SecondActivity.java
+++ app/src/main/java/com/jerry/githubdemo/SecondActivity.java
@@ -2,17 +2,14 @@ package com.jerry.githubdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+public class SecondActivity extends AppCompatActivity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        TextView textView = new TextView(this);
+        setContentView(R.layout.activity_second);
     }
 }
